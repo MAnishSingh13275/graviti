c5169d977a26891f345f2cf3f56eca9dec9c3fa6
Manish Singh <manishsingh13275@gmail.com>
2024-06-25 21:32:12 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 // pages/index.js
 import MapComponent from '../components/MapComponent';
 import Logo from '../assets/Graviti Logo 1.png'
+import Image from 'next/image';
 
 const Home = () => {
   return (
     <div>
       <header>
-        <img width={120} className='ml-5 py-2' src={Logo.src} alt="Graviti Logo" />
+        <Image width={120} height={100} className='ml-5 py-2' src={Logo.src} alt="Graviti Logo" />
       </header>
       <MapComponent />
     </div>
